85b9d1778666076a7c2f37348a080bab662254c6
Somesh Sontakke <sontakkesomesh@gmail.com>
2015-01-22 03:00:18 +0530

    Created a function to copy the data structure and not just copy a pointer to it, so that further manipulation can take place.

--- triangle.py
+++ triangle.py
@@ -41,8 +41,24 @@ for i in range(1,height+1):
 	input_tr.append(out_array1[i-1:(2*i-1)])
 #print input_tr
 
-output_tr = input_tr
+#checking if a simple equality copies the data structure or just copies the pointer
+output_tr1 = input_tr
+#input_tr[0][0] = 12
+#input_tr[0][0] - output_tr[0][0] <- this result is zero which impleies that only the pointer is copied.
+def copy_array(array1):
+	output_array1 = []
+	for i in range(0, len(array1)):
+		output_array1.append(array1[i])
+	return output_array1
+
+def copy_triangle(input_triangle):
+	output_triangle = []
+	for i in range(0,height):
+		output_triangle.append(copy_array(input_triangle[i]))
+	return output_triangle
+
+output_tr = copy_triangle(input_tr)
 input_tr[0][0] = 12
-print input_tr[0][0], output_tr[0][0]
+print output_tr, input_tr[0][0] - output_tr[0][0]
 		
 
